0cc41f34b5c25b98d5b7cf56c8c0cc437bce4c08
Derek Erdmann <derek@derekerdmann.com>
2017-07-08 12:23:50 -0700

    Switch image to just RGB

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ extern crate image;
 extern crate graphics;
 extern crate vecmath;
 
-use image::Rgba;
+use image::{ Rgb, ConvertBuffer };
 use graphics::math::{ Vec3d, Scalar };
 
 // Objects that can be placed in a scene
 struct Background {
-    color: Rgba<u8>
+    color: Rgb<u8>
 }
 
 struct Sphere {
     center: Vec3d,
     r: Scalar,
-    color: Rgba<u8>,
+    color: Rgb<u8>,
 }
 
 struct Floor {
@@ -24,12 +24,12 @@ struct Floor {
     bottomRight: Vec3d,
     normal: Vec3d,
     f: Scalar,
-    color: Rgba<u8>,
+    color: Rgb<u8>,
 }
 
 impl Floor {
 
-    fn new(bottomLeft: Vec3d, topLeft: Vec3d, topRight: Vec3d, bottomRight: Vec3d, color: Rgba<u8>) -> Floor {
+    fn new(bottomLeft: Vec3d, topLeft: Vec3d, topRight: Vec3d, bottomRight: Vec3d, color: Rgb<u8>) -> Floor {
 
         let (normal, f) = Floor::calculatenormal(bottomLeft, topLeft, bottomRight);
 
@@ -102,12 +102,12 @@ trait Traceable {
 
     // If the Ray intersects the shape, returns the distance from the Ray's
     // origin and the color at that point.
-    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Rgba<u8>)>;
+    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Rgb<u8>)>;
 }
 
 // The background object always intersects and returns its static color
 impl Traceable for Background {
-    fn intersect(&self, _: &Ray) -> Option<(Scalar, Rgba<u8>)> {
+    fn intersect(&self, _: &Ray) -> Option<(Scalar, Rgb<u8>)> {
         Some((std::f64::INFINITY, self.color))
     }
 }
@@ -121,7 +121,7 @@ impl Traceable for Sphere {
     ///
     /// \omega = (-B \pm \sqrt{B^2 - 4 * C}) / 2
     ///
-    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Rgba<u8>)> {
+    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Rgb<u8>)> {
 
         // B=2 * (dx(x_o −x_c)+dy(y_o −y_c)+dz(z_o −z_c)) 
         // which is just the dot product
@@ -166,7 +166,7 @@ impl Traceable for Floor {
     /// Plane intersection formula comes from CG II slides
     /// (2-2b-rt-basics-4.pdf).
     /// \omega = -(P_n . P_o + F) / (P+n . D)
-    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Rgba<u8>)> {
+    fn intersect(&self, ray: &Ray) -> Option<(Scalar, Rgb<u8>)> {
 
         let dist = -vecmath::vec3_dot(self.normal, ray.origin) + self.f /
                     vecmath::vec3_dot(self.normal, ray.direction);
@@ -193,26 +193,26 @@ impl Traceable for Floor {
 
 fn main() {
 
-    let background = Background { color: Rgba([0, 175, 215, 255]) };
+    let background = Background { color: Rgb([0, 175, 215]) };
 
     let sphere1 = Sphere {
         center: [-0.75, -0.5, 2.25],
         r: 0.45,
-        color: Rgba([255, 255, 0, 255])
+        color: Rgb([255, 255, 0])
         };
 
     let sphere2 = Sphere {
         center: [0.0, 0.0, 1.5],
         r: 0.5,
-        color: Rgba([0, 225, 0, 255])
+        color: Rgb([0, 225, 0])
         };
 
     let floor = Floor::new(
-        [ -2.0, -2.0, 0.0],
-        [ -2.0, 2.0, 0.0],
-        [ 2.0, 2.0, 0.0 ],
-        [ 2.0, -2.0, 0.0 ],
-        Rgba([255, 0, 0, 255])
+        [-2.0, -2.0, 0.0],
+        [-2.0, 2.0, 0.0],
+        [2.0, 2.0, 0.0],
+        [2.0, -2.0, 0.0],
+        Rgb([255, 0, 0])
     );
     let floor = floor.rotate_x( 75.0 );
     let floor = floor.translate( [-1.0, -1.25, 2.0] );
@@ -220,7 +220,7 @@ fn main() {
     let shapes: Vec<&Traceable> = vec![&sphere1, &sphere2, &floor, &background];
 
     // Create the raw image buffer
-    let mut image = image::RgbaImage::from_pixel(640, 640, Rgba([255, 0, 0, 255]));
+    let mut image = image::RgbImage::from_pixel(640, 640, Rgb([255, 0, 0]));
 
     // Trace through the scene
     //TODO build tracer
@@ -233,7 +233,7 @@ fn main() {
     // Generate a texture so the image buffer can be rendered to the screen
     let texture = piston_window::Texture::from_image(
             &mut window.factory,
-            &image,
+            &image.convert(),
             &piston_window::TextureSettings::new()
         ).unwrap();
 
