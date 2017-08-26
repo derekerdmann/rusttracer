3b4ecd5346f1bbcc894ba89c4ccae98650d21634
Derek Erdmann <derek@derekerdmann.com>
2017-08-26 13:29:56 -0700

    Add test for background intersections

--- src/tracer.rs
+++ src/tracer.rs
@@ -24,3 +24,35 @@ impl Traceable for Background {
         Some((std::f64::INFINITY, self.color))
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+
+    use std;
+    use image::Rgb;
+    use cgmath::{vec3};
+    use ray::Ray;
+    use tracer::{Traceable, Background};
+
+    // Tests that the background always intersects with any Ray
+    #[test]
+    fn background_always_intersects() {
+        let rays = [
+            Ray::new(vec3(0.0, 0.0, 0.0), vec3(1.0, 0.0, 0.0)),
+            Ray::new(vec3(0.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0)),
+            Ray::new(vec3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 1.0)),
+            Ray::new(vec3(0.0, 0.0, 0.0), vec3(-1.0, 0.0, 0.0)),
+            Ray::new(vec3(0.0, 0.0, 0.0), vec3(0.0, -1.0, 0.0)),
+            Ray::new(vec3(0.0, 0.0, 0.0), vec3(0.0, 0.0, -1.0)),
+        ];
+        
+        let bg = Background { color: Rgb([0, 175, 215]) };
+        for r in rays.iter() {
+            let (dist, color) = bg.intersect(r).expect("Background must always intersect");
+            assert_eq!(bg.color, color);
+            assert_ulps_eq!(std::f64::INFINITY, dist);
+        }
+    }
+
+}
